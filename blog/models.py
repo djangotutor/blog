462b08ef11371271aad0b7621a6a6e36184530ac
djangotutor <djangotutor@gmail.com>
2017-12-22 02:43:54 +0900

    Category 모델 작성및 테이블 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	order = models.IntegerField()
+	private_state = models.BooleanField(default=False)
+
+	def private(self):
+		self.private_state = not self.private
+		self.save()
+
+	def __str__(self):
+		self.name
