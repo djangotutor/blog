b61a87c3933fec7e0d7af8515323343a1a8074d0
djangotutor <djangotutor@gmail.com>
2017-12-22 22:59:36 +0900

    템플릿 카테고리 링크에 css 적용

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.Home.as_view(), name='home'),
 	url(r'^(?P<category>[ㄱ-ㅣ가-힣|a-zA-Z0-9]+)/$', views.PostList.as_view(), name='posts_by_category'),
+	url(r'^post/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from .models import Category
@@ -23,3 +24,19 @@ class PostList(Home):
 	def get_queryset(self):
 		self.category = get_object_or_404(Category, name=self.kwargs['category'])
 		return Post.objects.filter(category__private_state=False).filter(category__name=self.category).filter(published_date__lte=timezone.now()).order_by('published_date')
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['category_name'] = self.kwargs['category']
+		return context
+
+class PostDetail(DetailView):
+	model = Post
+	template_name = 'blog/post_detail.html'
+	context_object_name = 'post'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categorys'] = Category.objects.filter(private_state=False).order_by('order')
+		context['category_name'] = Category.objects.filter(post__id=self.kwargs['pk'])[0].name
+		return context
