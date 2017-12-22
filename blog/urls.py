13dec530eb583211bbb75ccfd5f1b1c5842c75f1
djangotutor <djangotutor@gmail.com>
2017-12-22 21:50:05 +0900

    포스트 출력및 정적파일 적용

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.PostList.as_view(), name='post_list'),
+	url(r'^$', views.Home.as_view(), name='home'),
+	url(r'^(?P<category>[ㄱ-ㅣ가-힣|a-zA-Z0-9]+)/$', views.PostList.as_view(), name='posts_by_category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
+from django.utils import timezone
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from .models import Category
+from .models import Post
 
-class PostList(ListView):
-	template_name = 'blog/post_list.html'
-	queryset = Category.objects.filter(private_state=False).order_by('order')
-	context_object_name = 'categorys'
+class Categorys(ListView):
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['categorys'] = Category.objects.filter(private_state=False).order_by('order')
+		return context
+
+class Home(Categorys):
+	teplate_name = 'blog/post_list.html'
+	context_object_name = 'posts'
+
+	def get_queryset(self):
+		return Post.objects.filter(category__private_state=False).filter(published_date__lte=timezone.now()).order_by('published_date')
+
+class PostList(Home):
+
+	def get_queryset(self):
+		self.category = get_object_or_404(Category, name=self.kwargs['category'])
+		return Post.objects.filter(category__private_state=False).filter(category__name=self.category).filter(published_date__lte=timezone.now()).order_by('published_date')
